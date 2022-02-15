6ee29782201384357d6217310f61b48d3ae8ab52
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2022-02-15 15:52:04 -0500

    Create landing page with signup and login buttons/routes

---
+++ client/src/Components/Signup Button/SignupButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import './SignupButton.scss';
+
+export default function SignupButton() {
+  return (
+    <div className='SignupButton-container'>
+      {/* <a className='btn btn-2'>Signup</a> */}
+      <Link className='btn btn-2' to='/signup'>Signup</Link>
+    </div>
+  );
+}
--- client/src/Pages/Landing/Landing.tsx
+++ client/src/Pages/Landing/Landing.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import './Landing.scss';
+import SignupButton from '../../Components/Signup Button/SignupButton';
+import LoginButton from '../../Components/Login Button/LoginButton';
 
 export default function Landing() {
-  return <div className={'Landing'}>Hello from Landing</div>;
+  return (
+    <div className='Landing-container'>
+      <div className='Landing-text'>
+        <h2>Welcome</h2>
+        <h3>Please select one of the following</h3>
+      </div>
+      <div className='Landing-buttons'>
+        <SignupButton />
+        <LoginButton />
+      </div>
+    </div>
+  );
 }
