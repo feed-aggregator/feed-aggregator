13183a1db913e3fa89c83b87a0357964de620840
leocrossman <lkcrossman@gmail.com>
2022-02-15 13:04:26 -0500

    got theme logic working through context

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,19 +2,18 @@ import * as React from 'react';
 import * as ReactDOM from "react-dom";
 
 import App from './pages/HelloWorld/HelloWorld';
-import "./styles.scss";
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
 
-import { ThemeProvider } from './Theme/ThemeContext';
-// import Background from './Theme/Background/Background';
+import { ThemeProvider, getInitialTheme } from './Theme/ThemeContext';
 
 import {Background} from './Theme/Background/Background'
+import './Theme/Background/Background.scss'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider initialTheme='dark' >
+      <ThemeProvider initialTheme={getInitialTheme()} >
         <Background>
           <App name='hello!' />
         </Background>
