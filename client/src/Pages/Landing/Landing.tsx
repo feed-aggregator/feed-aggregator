c1c803ef222cd734103a7bdcbd6d1c4a28fa0820
John Alicastro <93687275+JohnAlicastro@users.noreply.github.com>
2022-02-15 16:12:41 -0500

    Add dashboard button/link to route on landing page for Leo

--- client/src/Pages/Landing/Landing.tsx
+++ client/src/Pages/Landing/Landing.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Landing.scss';
+import { Link } from 'react-router-dom';
 import SignupButton from '../../Components/Signup Button/SignupButton';
 import LoginButton from '../../Components/Login Button/LoginButton';
 
 export default function Landing() {
   return (
     <div className='Landing-container'>
+      <Link to='/dashboard'>DASHBOARD</Link>
       <div className='Landing-text'>
         <h2>Welcome</h2>
         <h3>Please select one of the following</h3>
