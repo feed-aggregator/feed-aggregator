aab5c20cc8f7fbeaf04fd742bdf86e61c72d5765
leocrossman <lkcrossman@gmail.com>
2022-02-16 15:48:18 -0500

    changed page title

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -63,7 +63,7 @@ const config = {
     new HtmlWebpackPlugin({
       templateContent: ({ htmlWebpackPlugin }) =>
         '<!DOCTYPE html><html><head><meta charset="utf-8"><title>' +
-        htmlWebpackPlugin.options.title +
+        'HakrFood™' +
         '</title></head><body><div id="app"></div></body></html>',
       filename: 'index.html',
     }),
