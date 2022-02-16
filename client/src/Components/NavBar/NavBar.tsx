d151821bfad383e10bc257cba13f43588629ee5d
leocrossman <lkcrossman@gmail.com>
2022-02-16 12:47:32 -0500

    dark mode toggle added to nav plus logic for icons to dynamically change fill color based on theme

--- client/src/Components/NavBar/NavBar.tsx
+++ client/src/Components/NavBar/NavBar.tsx
@@ -1,14 +1,32 @@
-import React, {useState, useEffect} from 'react'
-import './NavBar.scss'
-
+import React, { useState, useEffect, useContext } from 'react';
+import './NavBar.scss';
+import HomeIcon from '../Icons/HomeIcon/HomeIcon';
+import ThemeToggle from '../Icons/ThemeToggle/ThemeToggle';
+import { ThemeContext } from '../../Theme/ThemeContext';
 export default function NavBar() {
-	return (
-		<nav className='NavBar-container'>
-			<ul>
-				<li>Home</li>
-				<li>Filter</li>
-				<li>Log out</li>
-			</ul>
-		</nav>
-	)
+  const iconSize = '1.5em';
+  const themeColor = { light: '#909aa3', dark: '#f5f6f7' };
+  const { theme } = useContext(ThemeContext);
+  const isDarkMode = () => theme === 'dark';
+  return (
+    <nav className='NavBar-container'>
+      <ul>
+        <li className='home'>
+          <HomeIcon
+            color={isDarkMode() ? themeColor.dark : themeColor.light}
+            size={iconSize}
+          />
+        </li>
+        <li>Filter</li>
+        <li className='theme-toggle'>
+          <ThemeToggle
+            color={isDarkMode() ? themeColor.dark : themeColor.light}
+            size={iconSize}
+            isDarkMode={isDarkMode}
+          />
+        </li>
+        <li className='logout'>Log out</li>
+      </ul>
+    </nav>
+  );
 }
