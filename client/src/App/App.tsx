5bb6e914410bbbd9683042d1a6d97b0d02999da5
leocrossman <lkcrossman@gmail.com>
2022-02-16 13:06:54 -0500

    changed routes back to original so that dashboard is now at the proper path

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.scss'
+import './App.scss';
 
 import Landing from '../Pages/Landing/Landing';
 import Dashboard from '../Pages/Dashboard/Dashboard';
 
 export default function App() {
-	return (
-		<div className={'bg'}>
-			<div className='App-container'>
-			<BrowserRouter>
-				<Routes>
-					{/* <Route path='/' element={<Landing />} /> */}
-					{/* <Route path='/' element={<Dashboard />} /> */}
-					<Route path='/' element={<Dashboard />} />
-				</Routes>
-			</BrowserRouter>
-			</div>
-		</div>
-	)
+  return (
+    <div className={'bg'}>
+      <div className='App-container'>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Landing />} />
+            <Route path='/dashboard' element={<Dashboard />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </div>
+  );
 }
