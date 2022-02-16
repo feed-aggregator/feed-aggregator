afb8ce5bc09915e6e90eb42798aff9330d149ae7
leocrossman <lkcrossman@gmail.com>
2022-02-16 13:57:29 -0500

    removed overflow-x so that there isnt whitespace to scroll horizontally to

--- client/src/Components/Post/Post.tsx
+++ client/src/Components/Post/Post.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import './Post.scss';
 import { HNPost } from '../../util/types';
-import { IconContext } from 'react-icons';
-import { FaHackerNewsSquare } from 'react-icons/fa';
+import HackerNewsIcon from '../Icons/HackerNewsIcon/HackerNewsIcon';
 export default function Post({ content }: { content: HNPost }) {
   const openInNewTab = (url: string): void => {
     const newWindow = window.open(url, '_blank', 'noopener,noreferrer');
@@ -19,14 +18,10 @@ export default function Post({ content }: { content: HNPost }) {
       className='Post-container'
       onClick={onClickUrl(`https://news.ycombinator.com/item?id=${content.id}`)}
     >
-      <IconContext.Provider value={{ color: '#FF6600', size: '2em' }}>
-        <div>
-          <FaHackerNewsSquare />
-        </div>
-      </IconContext.Provider>
+      <HackerNewsIcon />
       <h3>{content.score}</h3>
       <h2>{content.title}</h2>
-      {JSON.stringify(content)}
+      {/* {JSON.stringify(content)} */}
     </a>
   );
 }
